2a2f14ba1463da0e46c042c9548d2bec818e60b3
Sebastian Jadczak <sebastian-jadczak@wp.pl>
2019-09-14 21:27:37 +0200

    add textarea to SubPage programming

--- src/subpages/WriteTopic.js
+++ src/subpages/WriteTopic.js
@@ -1,8 +1,13 @@
 import React from "react";
+import '../styles/WriteTopic.css'
 
 const WriteTopic = () => {
     return (
-        <textarea className="NewTopic"></textarea>
+        <>
+            <textarea className="NewTopic"></textarea>
+
+            <div className="buttonNewTopic"> <button className="addNewTopic">Dodaj</button></div>
+        </>
     )
 }
 export default WriteTopic;
